7b26b10ceacef66e24bc02e476c9bed28ce273a5
59865 <1664125539@qq.com>
2022-01-06 16:12:33 +0800

    first commit

---
+++ js/api.js
@@ -0,0 +1,21 @@
+// 访问数据的API接口地址
+var baseApi = "http://127.0.0.1:11011"
+
+// ---------------教师---------------
+ // 添加试题
+ var apiAddSubject = baseApi + "/add/subject"
+ 
+ // 查看题库中的所有题目
+ var apiShowSubjects = baseApi + "/all"
+ 
+ // 删除试题
+ var apiDelSubject = baseApi + "/del/subject"
+ 
+ // 修改试题
+ var apiUpdateSubject = baseApi + "/update/subject"
+ 
+ // 设置考试起止时间
+ var apiSetTime = baseApi + "/set/time"
+ 
+ // 生成试卷
+ var apiGenerate = baseApi + "/generate"
